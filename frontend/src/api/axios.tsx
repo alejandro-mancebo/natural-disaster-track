d8516a98888a09e2dfd763df2e217d7067b44359
alejandro-mancebo <lazaro.manceboariosa@georgebrown.ca>
2023-11-24 23:39:12 -0500

    Add render.yaml file

--- frontend/src/api/axios.tsx
+++ frontend/src/api/axios.tsx
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const URL = 'http://localhost:5000'
+const URL = import.meta.env.BASE_URL
+  ? import.meta.env.BASE_URL
+  : 'http://localhost:5000'
 
 export const axiosPublic = axios.create({
   baseURL: URL
