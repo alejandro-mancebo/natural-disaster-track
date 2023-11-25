bc2ad538759f4f8b971b51c485f747a214f76536
alejandro-mancebo <lazaro.manceboariosa@georgebrown.ca>
2023-11-25 17:15:23 -0500

    modify ip address to access Atlas closter

--- backend/src/config/allowedOrigins.js
+++ backend/src/config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
-  'https://natural-disaster-track.onrender.com',
+  // 'https://natural-disaster-track.onrender.com',
+  'https://ittaylor-natural-disaster-track.onrender.com',
   'https://ittaylor-api-ndt.onrender.com',
   'http://127.0.0.1:5000',
   'http://localhost:5173'
